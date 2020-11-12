a5b523552e5ec6ac383b6a98c0adc5539184bdba
Marcin Czernecki <Marcin_Czernecki@epam.com>
2020-11-12 16:49:44 +0100

    passwords hash gen

---
+++ PassStorage2.HashGen/PasswordGenerator.cs
@@ -0,0 +1,37 @@
+﻿using PassStorage2.Base.DataCryptoLayer;
+using System;
+using System.Windows.Forms;
+
+namespace PassStorage2.HashGen
+{
+    public partial class PasswordGenerator : Form
+    {
+        public PasswordGenerator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(tbHash.Text);
+                MessageBox.Show("Copied successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Copied with error {Environment.NewLine} {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tbPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                tbHash.Text = SHA.GenerateSHA256String(tbPassword.Text);
+            }
+            catch(Exception)
+            { }
+        }
+    }
+}
---
+++ PassStorage2.HashGen/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PassStorage2")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("d8be4aae-9e7b-4c9c-b918-745894960259")]
